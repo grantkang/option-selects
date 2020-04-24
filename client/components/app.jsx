f577a4032fca94e275755f33d129357a473f0c9d
Grant Kang <3s.grantkang@gmail.com>
2020-04-24 13:15:31 -0700

    App component now calls the 'getCartItems' method upon mounting

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -16,6 +16,10 @@ export default class App extends React.Component {
     };
   }
 
+  componentDidMount() {
+    this.getCartItems();
+  }
+
   getCartItems() {
     fetch('/api/cart')
       .then(response => response.json())
