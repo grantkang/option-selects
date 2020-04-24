b871bf6e8a0d7e2866052d0994105988b9a3daa2
Grant Kang <3s.grantkang@gmail.com>
2020-04-24 01:49:52 -0700

    ProductList component recieves/passes App.prototype.setView from App/to ProductListItem via props

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -8,9 +8,44 @@ export default class ProductDetails extends React.Component {
     };
   }
 
+  componentDidMount() {
+    fetch(`/api/products/${this.props.params.productId}`)
+      .then(response => {
+        return response.json();
+      })
+      .then(product => this.setState({ product: product }));
+  }
+
   render() {
-    return (
-      null
-    );
+    const product = this.state.product;
+    if (product) {
+      return (
+        <div className="p-4 bg-light">
+          <div className="card m-4">
+            <div className="card-body">
+              <span className="pointer" onClick={this.props.onClick}>&#60; back to catalog</span>
+              <div className="row">
+                <div className="col-5">
+                  <img className="card-img-top card-img my-3" src={product.image} alt=""/>
+                </div>
+                <div className="col-7">
+                  <h3 className="card-title">{product.name}</h3>
+                  <p className="card-text text-muted">{`$${(product.price / 100).toFixed(2)}`}</p>
+                  <p className="card-text">{product.shortDescription}</p>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-12">
+                  <p className="card-text">{product.longDescription}</p>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return null;
+    }
   }
 }
