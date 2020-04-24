d2aa07fd385335b89b17c212ecca64b1c7c91172
Grant Kang <3s.grantkang@gmail.com>
2020-04-24 13:17:41 -0700

    fixed an issue where the cart was being stored in the wrong property of state

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -10,9 +10,9 @@ export default class App extends React.Component {
     this.state = {
       view: {
         name: 'catalog',
-        params: {},
-        cart: []
-      }
+        params: {}
+      },
+      cart: []
     };
   }
 
