36c02a0a2cef5b305c9cc1a56b545aa936c5f60f
Grant Kang <3s.grantkang@gmail.com>
2020-04-24 15:16:50 -0700

    defined class for CartSummaryItem component

---
+++ client/components/cart-summary-item.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class CartSummaryItem extends React.Component {
+  render() {
+    const cartItem = this.props.cartItem;
+    return (
+      <div className="card">
+        <div className="row">
+          <div className="col-5">
+            <img className="card-img-top card-img my-3" src={cartItem.image} alt="" />
+          </div>
+          <div className="col-7 d-flex flex-column justify-content-center">
+            <h3 className="card-title">{cartItem.name}</h3>
+            <p className="card-text text-muted">{`$${(cartItem.price / 100).toFixed(2)}`}</p>
+            <p className="card-text">{cartItem.shortDescription}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
