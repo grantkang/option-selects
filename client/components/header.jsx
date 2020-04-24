c7533bd759a621cfc21c92bc72d1fb7177fc777d
Grant Kang <3s.grantkang@gmail.com>
2020-04-23 18:28:41 -0700

    updated styling

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -4,7 +4,7 @@ export default class Header extends React.Component {
   render() {
     return (
       <nav className="navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="#"><i className="fas fa-dollar-sign"></i>Wicked Sales</a>
+        <a className="navbar-brand" href="#"><i className="fas fa-dollar-sign"></i> Wicked Sales</a>
       </nav>
     );
   }
