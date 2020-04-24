230542485d71ce6f75d2a14a2fb26208f65d2096
Grant Kang <3s.grantkang@gmail.com>
2020-04-24 10:48:58 -0700

    added GET endpoint for '/api/cart' returns an empty array for now

--- server/index.js
+++ server/index.js
@@ -69,6 +69,10 @@ app.get('/api/products/:productId', (req, res, next) => {
     });
 });
 
+app.get('/api/cart', (req, res, next) => {
+  res.status(200).json([]);
+});
+
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
 });
