e45cca062eda31f667ebf7e0d362ff5142546499
Grant Kang <3s.grantkang@gmail.com>
2020-04-23 17:01:39 -0700

    ProductListItem component now gets data passed via props

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 
 export default class ProductListItem extends React.Component {
   render() {
+    const name = this.props.name;
+    const price = `$${this.props.price / 100}`;
+    const imagePath = this.props.imagePath;
+    const description = this.props.description;
+
     return (
       <div className="col mb-4">
         <div className="card">
-          <img className="card-img-top" src="" alt="Product Image" />
+          <img className="card-img-top" src={imagePath} alt="Product Image" />
           <div className="card-body">
-            <h5 className="card-title">Product Name</h5>
-            <p className="card-text"><small className="text-muted">Price</small></p>
-            <p className="card-text">Product Description</p>
+            <h5 className="card-title">{name}</h5>
+            <p className="card-text"><small className="text-muted">{price}</small></p>
+            <p className="card-text">{description}</p>
           </div>
         </div>
       </div>
