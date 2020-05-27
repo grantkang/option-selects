a5cc3416f244a5f29c9c81c790811e130174507b
Grant Kang <3s.grantkang@gmail.com>
2020-05-27 12:29:11 -0700

    ProductListItems now displayed in 2 columns when screen size is medium/large

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -8,14 +8,17 @@ export default class ProductListItem extends React.Component {
     const description = this.props.description;
 
     return (
-      <div className="product-list-item-card card pointer col-md-3 m-2 col-sm-12" onClick={this.props.onClick}>
-        <img className="card-img-top card-img my-3" src={imagePath} alt="Product Image" />
-        <div className="card-body">
-          <h5 className="card-title">{name}</h5>
-          <p className="card-text"><small className="text-muted">{price}</small></p>
-          <p className="card-text">{description}</p>
+      <div className="col-xl-4 col-md-6 col-sm-12 my-2">
+        <div className="product-list-item-card card pointer m-2" onClick={this.props.onClick}>
+          <img className="card-img-top card-img my-3" src={imagePath} alt="Product Image" />
+          <div className="card-body">
+            <h5 className="card-title">{name}</h5>
+            <p className="card-text"><small className="text-muted">{price}</small></p>
+            <p className="card-text">{description}</p>
+          </div>
         </div>
       </div>
+
     );
   }
 }
