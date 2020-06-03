7c7c5d0a295b62853d7b3a516c51a8840cd2a603
Grant Kang <3s.grantkang@gmail.com>
2020-06-02 20:55:40 -0700

    Header component now uses context to get application name

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
+import ApplicationContext from '../lib/context';
 
 export default function Header(props) {
   const history = useHistory();
+  const context = useContext(ApplicationContext);
   const cartItemCount = props.cartItemCount;
   const cartItemCountText = cartItemCount === 1 ? '1 item' : `${cartItemCount} items`;
 
   return (
     <nav className="navbar navbar-dark bg-dark">
-      <a className="navbar-brand pointer" onClick={() => history.push('/')}><i className="fas fa-dollar-sign"></i> Wicked Sales</a>
+      <a className="navbar-brand pointer" onClick={() => history.push('/')}><i className="fas fa-dollar-sign"></i> {context.getApplicationTitle()}</a>
       <span
         className="navbar-text pointer"
         onClick={() => history.push('/cart')}>
