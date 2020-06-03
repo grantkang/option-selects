f243c493945ee5692b5304fb82b9cabfc410acbc
Grant Kang <3s.grantkang@gmail.com>
2020-06-02 18:15:31 -0700

    converted CheckoutForm component into a function component & it now uses React Context & Router

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -1,82 +1,87 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import ApplicationContext from '../lib/context';
+import { useHistory } from 'react-router-dom';
 
-export default class CheckoutForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.state = {
-      name: '',
-      creditCard: '',
-      shippingAddress: '',
-      creditCardError: false
-    };
-  }
+export default function CheckoutForm(props) {
+  const history = useHistory();
+  const context = useContext(ApplicationContext);
+  const [state, setState] = useState({
+    name: '',
+    creditCard: '',
+    shippingAddress: '',
+    creditCardError: false
+  });
 
-  handleChange(e) {
-    const input = {};
-    input[e.target.name] = e.target.value;
-    this.setState(input);
-  }
+  const cart = context.getCart();
+  const totalPrice = cart.reduce((acc, item) => {
+    return acc + item.price;
+  }, 0);
 
-  handleSubmit(e) {
+  const handleChange = e => {
+    const newState = Object.assign({}, state);
+    newState[e.target.name] = e.target.value;
+    setState(newState);
+  };
+
+  const handleSubmit = e => {
     e.preventDefault();
-    if (!this.creditCardIsValid()) {
-      this.setState({ creditCardError: true });
+    if (!creditCardIsValid()) {
+      const newState = Object.assign({}, state);
+      newState.creditCardError = true;
+      setState(newState);
       return;
     }
-    const orderInfo = Object.assign({}, this.state);
-    this.props.placeOrder(orderInfo);
-  }
+    const orderInfo = Object.assign({}, state);
+    context.placeOrder(orderInfo);
+    history.push('/');
+  };
 
-  creditCardIsValid() {
-    return this.state.creditCard.match(/\d{4}-?\d{4}-?\d{4}-?\d{4}/);
-  }
+  const creditCardIsValid = () => {
+    return state.creditCard.match(/\d{4}-?\d{4}-?\d{4}-?\d{4}/);
+  };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <h1>Checkout</h1>
-        <span>{`Order Total: $${(this.props.params.orderTotal / 100).toFixed(2)}`}</span>
-        <div className="form-group">
-          <label htmlFor="nameInput">Name</label>
-          <input
-            required
-            type="text"
-            name="name"
-            className="form-control"
-            id="nameInput"
-            value={this.state.name}
-            onChange={this.handleChange}/>
-        </div>
-        <div className="form-group">
-          <label htmlFor="creditCardInput">Credit Card</label>
-          <input
-            required
-            type="text"
-            name="creditCard"
-            className={this.state.creditCardError ? 'form-control is-invalid' : 'form-control'}
-            id="creditCardInput"
-            value={this.state.creditCard}
-            onChange={this.handleChange}></input>
-          {this.state.creditCardError ? <small className="text-danger">Please input a valid credit card number</small> : null }
-        </div>
-        <div className="form-group">
-          <label htmlFor="addressInput">Shipping Address</label>
-          <textarea
-            required
-            name="shippingAddress"
-            className="form-control"
-            id="addressInput"
-            rows="4"
-            value={this.state.shippingAddress}
-            onChange={this.handleChange}></textarea>
-        </div>
-        <div className="d-flex align-items-center justify-content-between">
-          <span className="pointer" onClick={() => this.props.setView('catalog', {})}>&#60; Continue Shopping</span>
-          <button type="submit" className="btn btn-primary">Place Order</button>
-        </div>
-      </form>
-    );
-  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Checkout</h1>
+      <span>{`Order Total: $${(totalPrice / 100).toFixed(2)}`}</span>
+      <div className="form-group">
+        <label htmlFor="nameInput">Name</label>
+        <input
+          required
+          type="text"
+          name="name"
+          className="form-control"
+          id="nameInput"
+          value={state.name}
+          onChange={handleChange}/>
+      </div>
+      <div className="form-group">
+        <label htmlFor="creditCardInput">Credit Card</label>
+        <input
+          required
+          type="text"
+          name="creditCard"
+          className={state.creditCardError ? 'form-control is-invalid' : 'form-control'}
+          id="creditCardInput"
+          value={state.creditCard}
+          onChange={handleChange}></input>
+        {state.creditCardError ? <small className="text-danger">Please input a valid credit card number</small> : null }
+      </div>
+      <div className="form-group">
+        <label htmlFor="addressInput">Shipping Address</label>
+        <textarea
+          required
+          name="shippingAddress"
+          className="form-control"
+          id="addressInput"
+          rows="4"
+          value={state.shippingAddress}
+          onChange={handleChange}></textarea>
+      </div>
+      <div className="d-flex align-items-center justify-content-between">
+        <span className="pointer" onClick={() => history.push('/')}>&#60; Continue Shopping</span>
+        <button type="submit" className="btn btn-primary">Place Order</button>
+      </div>
+    </form>
+  );
 }
