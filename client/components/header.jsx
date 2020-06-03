97b256c7ec2a37af6358c4d7c801610caf448b16
Grant Kang <3s.grantkang@gmail.com>
2020-06-02 22:27:38 -0700

    Header component now uses Material-UI components

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,21 +1,51 @@
 import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+
 import ApplicationContext from '../lib/context';
+import { Toolbar, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  titleContainer: {
+    flexGrow: 1
+  },
+  title: {
+    '&:hover': {
+      cursor: 'pointer'
+    }
+  },
+  icon: {
+    color: theme.palette.primary.contrastText
+  }
+}));
 
 export default function Header(props) {
+  const classes = useStyles();
   const history = useHistory();
   const context = useContext(ApplicationContext);
   const cartItemCount = props.cartItemCount;
   const cartItemCountText = cartItemCount === 1 ? '1 item' : `${cartItemCount} items`;
 
   return (
-    <nav className="navbar navbar-dark bg-dark">
-      <a className="navbar-brand pointer" onClick={() => history.push('/')}><i className="fas fa-dollar-sign"></i> {context.getApplicationTitle()}</a>
-      <span
-        className="navbar-text pointer"
-        onClick={() => history.push('/cart')}>
-        {cartItemCountText} <i className="fas fa-shopping-cart"></i>
-      </span>
-    </nav>
+    <AppBar position="sticky">
+      <Toolbar>
+        <IconButton edge="start" aria-label="menu">
+          <MenuIcon className={classes.icon}/>
+        </IconButton>
+        <div className={classes.titleContainer}>
+          <Typography variant="h6" className={classes.title} display="inline" noWrap onClick={() => history.push('/')}>
+            {context.getApplicationTitle()}
+          </Typography>
+        </div>
+        <Typography variant="h6">{cartItemCountText}</Typography>
+        <IconButton area-label="cart" onClick={() => history.push('/cart')}>
+          <ShoppingCartIcon className={classes.icon}/>
+        </IconButton>
+      </Toolbar>
+    </AppBar>
   );
 }
