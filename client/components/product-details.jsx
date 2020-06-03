68f8f4a512f1f4ebc36a59f56e0ca42469803874
Grant Kang <3s.grantkang@gmail.com>
2020-06-02 17:09:55 -0700

    converted ProductDetails component into a function component & it now uses React Context & Router

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -1,49 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import ApplicationContext from '../lib/context';
 
-export default class ProductDetails extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      product: null
-    };
-  }
+export default function ProductDetails(props) {
+  const [product, setProduct] = useState({});
+  const history = useHistory();
+  const { id } = useParams();
+  const context = useContext(ApplicationContext);
 
-  componentDidMount() {
-    fetch(`/api/products/${this.props.params.productId}`)
+  useEffect(() => {
+    fetch(`/api/products/${id}`)
       .then(response => {
         return response.json();
       })
-      .then(product => this.setState({ product: product }));
-  }
+      .then(product => setProduct(product));
+  }, []);
 
-  render() {
-    const product = this.state.product;
-    if (product) {
-      return (
-        <div className="card m-4">
-          <div className="card-body">
-            <span className="pointer" onClick={() => { this.props.setView('catalog', {}); }}>&#60; back to catalog</span>
-            <div className="row">
-              <div className="col-5">
-                <img className="card-img-top card-img my-3" src={product.image} alt=""/>
-              </div>
-              <div className="col-7">
-                <h3 className="card-title">{product.name}</h3>
-                <p className="card-text text-muted">{`$${(product.price / 100).toFixed(2)}`}</p>
-                <p className="card-text">{product.shortDescription}</p>
-                <button className="btn btn-primary" onClick={() => this.props.addToCart(product)}>Add to Cart</button>
-              </div>
+  if (product) {
+    return (
+      <div className="card m-4">
+        <div className="card-body">
+          <span className="pointer" onClick={() => { history.push('/'); }}>&#60; back to catalog</span>
+          <div className="row">
+            <div className="col-5">
+              <img className="card-img-top card-img my-3" src={product.image} alt=""/>
             </div>
-            <div className="row">
-              <div className="col-12">
-                <p className="card-text">{product.longDescription}</p>
-              </div>
+            <div className="col-7">
+              <h3 className="card-title">{product.name}</h3>
+              <p className="card-text text-muted">{`$${(product.price / 100).toFixed(2)}`}</p>
+              <p className="card-text">{product.shortDescription}</p>
+              <button className="btn btn-primary" onClick={() => context.addToCart(product)}>Add to Cart</button>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-12">
+              <p className="card-text">{product.longDescription}</p>
             </div>
           </div>
         </div>
-      );
-    } else {
-      return null;
-    }
+      </div>
+    );
+  } else {
+    return null;
   }
 }
