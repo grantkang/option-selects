fc9df323199466ee7cd386844280c56478734c4c
Grant Kang <3s.grantkang@gmail.com>
2020-06-15 13:24:37 -0700

    ProductList component no longer calls back-end & now receives ProductListItems as props

--- client/components/product-list.jsx
+++ client/components/product-list.jsx
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import ProductListItem from './product-list-item';
 import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles(theme => ({
@@ -14,33 +12,10 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export default function ProductList(props) {
-  const [products, setProducts] = useState([]);
-  const history = useHistory();
   const classes = useStyles();
-
-  useEffect(() => {
-    fetch('/api/products')
-      .then(response => response.json())
-      .then(products => {
-        setProducts(products);
-      });
-  }, []);
-
-  const productListItems = products.map(product => {
-    return (
-      <ProductListItem
-        key={product.productId}
-        name={product.name}
-        price={product.price}
-        imagePath={product.imagePath}
-        onClick={() => history.push(`/products/${product.productId}`)}
-      />
-    );
-  });
   return (
     <Grid container direction="row" justify="space-around" spacing={4} className={classes.root}>
-      {productListItems}
+      {props.children}
     </Grid>
   );
-
 }
