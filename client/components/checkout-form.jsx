8d1951b418705cb9db26461a4234f03e619feb15
Grant Kang <3s.grantkang@gmail.com>
2020-06-16 00:57:35 -0700

    minimized use of treeshaking

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -4,7 +4,10 @@ import { useHistory } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import { Box, Button, TextField, Grid } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles(theme => ({
   root: {
