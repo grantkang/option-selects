0815c83ef82377b8abd8a6196e03088ec2f28927
Grant Kang <3s.grantkang@gmail.com>
2020-06-16 00:14:45 -0700

    converted to functional component and made use of hooks

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -1,21 +1,59 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 
-export default class CartSummaryItem extends React.Component {
-  render() {
-    const cartItem = this.props.cartItem;
-    return (
-      <div className="card my-2">
-        <div className="row">
-          <div className="col-5">
-            <img className="card-img-top card-img my-3" src={cartItem.image} alt="" />
-          </div>
-          <div className="col-7 d-flex flex-column justify-content-center">
-            <h3 className="card-title">{cartItem.name}</h3>
-            <p className="card-text text-muted">{`$${(cartItem.price / 100).toFixed(2)}`}</p>
-            <p className="card-text">{cartItem.shortDescription}</p>
-          </div>
-        </div>
-      </div>
-    );
+const useStyles = makeStyles(theme => ({
+  root: {
+
+  },
+  image: {
+    width: '100%',
+    objectFit: 'scale-down'
+  },
+  mainImageContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    height: '200px',
+    padding: theme.spacing(3)
+  },
+  productDetails: {
+    '& > *': {
+      margin: theme.spacing(2, 0)
+    }
   }
+}));
+
+export default function CartSummaryItem(props) {
+  const classes = useStyles();
+  const cartItem = props.cartItem;
+  return (
+    <Paper>
+      <Grid container>
+        <Grid item xs={12} md={5}>
+          <div className={classes.mainImageContainer}>
+            <img className={classes.image} src={cartItem.imagePath} alt="" />
+          </div>
+        </Grid>
+        <Grid className={classes.productDetails} item xs={12} md={6}>
+          <Typography variant="h5">{cartItem.name}</Typography>
+          <Typography variant="h4">
+            {`$${(cartItem.price / 100).toFixed(2)}`}
+          </Typography>
+          {cartItem.colorId ? (
+            <div>
+              <Typography display="inline" variant="button">Color: {cartItem.colorName}</Typography>
+            </div>
+          ) : null}
+          {cartItem.sizeId ? (
+            <div>
+              <Typography display="inline" variant="button">Size: {cartItem.sizeAbreviation}</Typography>
+            </div>
+          ) : null}
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+
 }
